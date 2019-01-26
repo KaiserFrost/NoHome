d87ff55ee0a2d7b46a31855eaa50851580772196
Unknown <carlos-.-alves@hotmail.com>
2019-01-26 11:16:09 +0000

    Menu, Animator

---
+++ NoHome/Assets/Scripts/MainMENU1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMENU1 : MonoBehaviour {
+
+  
+
+    public void PlayGame(int sceneindex)
+    {
+
+        SceneManager.LoadScene(sceneindex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
